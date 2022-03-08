4a2faf3214b06fecb779a3ca66bf02353a499810
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-03-08 15:06:28 -0400

    server(huggingface): refactor with new models

--- server/main.py
+++ server/main.py
@@ -1,18 +1,18 @@
 import requests
+from input import DialoGPTInput, Inputs
+from response import DialoGPTResponse, dialo_gpt_response_from_dict
 
-json_data = {
-    'inputs': {
-        'generated_responses': [
-            'I\'m good, how are you?',
-        ],
-        'past_user_inputs': [
-            'Hey my name is Julien! How are you?',
-        ],
-        'text': 'Good good thanks',
-    },
-}
 
-response = requests.post(
-    'https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium', json=json_data)
-print(response.text)
-# {"generated_text":"No problem, I'm glad you're doing well!","conversation":{"generated_responses":["I'm good, how are you?","No problem, I'm glad you're doing well!"],"past_user_inputs":["Hey my name is Julien! How are you?","Good good thanks"]},"warnings":["Setting `pad_token_id` to `eos_token_id`:50256 for open-end generation."]}
+def query_huggingface_dialogpt(json_data: DialoGPTInput) -> DialoGPTResponse:
+    response = requests.post(
+        'https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium', json=json_data.to_dict())
+    json_dict = response.json()
+    result = dialo_gpt_response_from_dict(json_dict)
+    return result  #
+
+
+json_data = DialoGPTInput(inputs=Inputs(
+    generated_responses=["I\'m good, how are you?"], past_user_inputs=["Hey my name is Julien! How are you?"], text="Good good thanks"))
+
+response = query_huggingface_dialogpt(json_data)
+print(response.conversation.generated_responses)
