64f05539f7691618ea6d56515af56d714bfdae10
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-03-09 14:44:04 -0400

    server: single turn conversation

--- server/main.py
+++ server/main.py
@@ -1,14 +1,37 @@
 import uvicorn
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
+from event import event_from_dict
+from huggingface import query_huggingface_dialogpt
+from input import DialoGPTInput, Inputs
+from stream_chat import StreamChat
+import os
+your_api_key = os.environ.get('api_key')
+your_api_secret = os.environ.get('api_secret')
 
+
+chat = StreamChat(api_key=your_api_key, api_secret=your_api_secret)
 app = Starlette(debug=True)
+
 
 @app.route('/chatEvent', methods=['POST'])
 async def chatEvent(request):
-    data = request.json
-    print(data)
-    return JSONResponse({"data": "message sent"})
+    print("new event received")
+    data = await request.json()
+    event = event_from_dict(data)
+    if event.type == "message.new" and event.message.user.id != "eugene-goostman":
+        print("new user message")
+        channel = chat.channel("messaging", "my-ai-friend")
+        json_data = DialoGPTInput(inputs=Inputs(
+            generated_responses=[], past_user_inputs=[], text=event.message.text))
+
+        response = query_huggingface_dialogpt(json_data)
+        generated_responses = response.conversation.generated_responses
+        array_length = len(generated_responses)
+        last_generated_response = generated_responses[array_length - 1]
+        channel.send_message(
+            {"text": last_generated_response}, "eugene-goostman")
+    return JSONResponse({"text": "revent received"})
 
 
 if __name__ == "__main__":
