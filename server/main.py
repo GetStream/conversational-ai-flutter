6dff92392dcd104ebd55d203482af561993570ad
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-03-09 11:30:08 -0400

    server: setup starlette

--- server/main.py
+++ server/main.py
@@ -1,18 +1,15 @@
-import requests
-from input import DialoGPTInput, Inputs
-from response import DialoGPTResponse, dialo_gpt_response_from_dict
+import uvicorn
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+
+app = Starlette(debug=True)
+
+@app.route('/chatEvent', methods=['POST'])
+async def chatEvent(request):
+    data = request.json
+    print(data)
+    return JSONResponse({"data": "message sent"})
 
 
-def query_huggingface_dialogpt(json_data: DialoGPTInput) -> DialoGPTResponse:
-    response = requests.post(
-        'https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium', json=json_data.to_dict())
-    json_dict = response.json()
-    result = dialo_gpt_response_from_dict(json_dict)
-    return result  #
-
-
-json_data = DialoGPTInput(inputs=Inputs(
-    generated_responses=["I\'m good, how are you?"], past_user_inputs=["Hey my name is Julien! How are you?"], text="Good good thanks"))
-
-response = query_huggingface_dialogpt(json_data)
-print(response.conversation.generated_responses)
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
